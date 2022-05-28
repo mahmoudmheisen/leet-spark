55bb9a02fd52dd6b9ca457e03cfe9bf03b2beb3d
mmh91 <mahmoudmheisen91@gmail.com>
2022-05-28 22:11:48 +0300

    Time: 26 ms (17.87%), Space: 9.1 MB (16.56%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -11,20 +11,17 @@ public:
 class Solution {
 public:
   string frequencySort(string s) {
-    priority_queue<pqPair, vector<pqPair>, compare> pq;
     map<char, int> dict;
+    priority_queue<pqPair, vector<pqPair>, compare> pq;
+    stringstream ss;
+    string res;
     
     for (char c : s) {
       if (!dict[c]) dict[c] = 1;
       else dict[c]++;
     }
     
-    for (pqPair pair : dict) {
-      pq.push(pair);
-    }
-    
-    stringstream ss;
-    string res;
+    for (pqPair pair : dict) pq.push(pair);
     
     while (pq.size()) {
       for (int i = 0; i < pq.top().second; i++) ss << pq.top().first;
