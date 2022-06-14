2100ecfc51024a00aee70b1a980001f1effc8ed2
mmh91 <mahmoudmheisen91@gmail.com>
2022-06-14 10:08:33 +0300

    Time: 123 ms (23.73%), Space: 36.3 MB (15.20%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -4,8 +4,8 @@ public:
       int result = 0;
       int idx = 0;
 
-      if (ruleKey == "color") idx = 1;
-      if (ruleKey == "name") idx = 2;
+      if (ruleKey[0] == 'c') idx = 1;
+      if (ruleKey[0] == 'n') idx = 2;
       
       for (auto item : items) {
         if (item[idx] == ruleValue) result++;
