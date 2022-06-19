66951b1693a9923b95762399fe8824d61da4d4a8
mmh91 <mahmoudmheisen91@gmail.com>
2022-06-19 11:11:54 +0300

    Time: 0 ms (100.00%), Space: 5.9 MB (60.57%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -7,16 +7,20 @@ public:
       
       if (n < k) return 0;
       
-      string temp = "";
-      for (int i = 0; i < n; i++) {
-        temp += s[i];
-        
-        if (temp.size() == k) {
-          int x = stoi(temp);
-          
-          if (x && num % x == 0) count++;
-          temp.erase(0, 1);
-        }
+      int current_beauty = stoi(s.substr(0, k));
+      
+      if (current_beauty && num % current_beauty == 0) count++;
+      
+      int factor = 1;
+      for (int i = 1; i < k; i++) {
+        factor *= 10;
+      }
+      
+      for (int i = k; i < n; i++) {
+        current_beauty -= (s[i-k] - '0')*factor;
+        current_beauty *= 10;
+        current_beauty += (s[i] - '0');
+        if (current_beauty && num % current_beauty == 0) count++;
       }
       
       return count;
