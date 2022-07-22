5cfddcf07e4ebce82732cbf37c4f3a9c8494d05e
mmh91 <mahmoudmheisen91@gmail.com>
2022-07-22 19:16:30 +0300

    Time: 73 ms (80.17%), Space: 30.2 MB (34.68%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // bucket sort
+        vector<int> res;
         
+        // min, max
+        int max = *max_element(nums.begin(), nums.end());
+        int min = *min_element(nums.begin(), nums.end());
+
+        vector<int> bucket(max - min + 1);
+        for (int i : nums) {
+            bucket[i - min]++;
+        }
         
-        return nums;
+        for (int i=0; i < bucket.size(); i++) {
+            while(bucket[i]>0) {
+                res.push_back(i+min);
+                bucket[i]--;
+            }
+        }
+
+        return res;
     }
 };
