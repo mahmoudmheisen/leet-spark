18dc2444679323829302babd425e713cf2e17b70
mmh91 <mahmoudmheisen91@gmail.com>
2022-07-25 23:01:05 +0300

    Time: 202 ms (35.00%), Space: 87.2 MB (5.12%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.cpp
@@ -0,0 +1,28 @@
+class OrderedStream {
+public:
+    int size;
+    map<int, string> m;
+    int ptr;
+    OrderedStream(int n) {
+        size = n;
+        ptr = 1;
+    }
+    
+    vector<string> insert(int idKey, string value) {
+        m[idKey] = value;
+        
+        vector<string> vec;
+        for(int i = 0; i < size; i++) {
+            if(m[ptr] != "") {vec.push_back(m[ptr]); ptr++;}
+            else break;
+        }
+        
+        return vec;
+    }
+};
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream* obj = new OrderedStream(n);
+ * vector<string> param_1 = obj->insert(idKey,value);
+ */
