7185b1da8c065358363cda3a340b570ae3764ea5
mmh91 <mahmoudmheisen91@gmail.com>
2022-07-26 07:17:41 +0300

    Time: 11 ms (5.49%), Space: 6.6 MB (48.88%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     bool checkIfPangram(string s) {
-        if(s.size() < 26) return false;
-        map<int, int> m;
+        map<char, int> m;
         
-        for(char c: s) {
-            m[c - 97]++;
-        }
-        
+        for(char c: s) m[c]++;
         if(m.size() < 26) return false;
         
         return true;
