1a7381e186e15aa8ef932220a0ffb916a6411d4a
mmh91 <mahmoudmheisen91@gmail.com>
2023-02-27 23:55:33 +0300

    Time: 296 ms (44.62%), Space: 141 MB (14.33%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-    void count(vector<vector<int>>& grid, bool countRow, vector<int>& zeros, vector<int>& ones) {
+    void count(vector<vector<int>>& grid, bool countRow, vector<int>& ones) {
         int m = grid.size();
         int n = grid[0].size();
 
@@ -8,26 +8,23 @@ private:
         int l = countRow ? n : m;
 
         for(int i = 0; i < k; i++) {
-            int s0 = 0;
-            int s1 = 0;
+            int sum = 0;
 
             for(int j = 0; j < l; j++) {
                 int val = countRow ? grid[i][j] : grid[j][i];
-                s0 += val == 0 ? 1 : 0;
-                s1 += val;
+                sum += val;
             }
-            zeros.push_back(s0);
-            ones.push_back(s1);
+            ones.push_back(sum);
         }
     }
 
 public:
     vector<vector<int>> onesMinusZeros(vector<vector<int>>& grid) {
         vector<vector<int>> diff;
-        vector<int> r0, r1, c0, c1;
+        vector<int> r, c;
     
-        count(grid, true, r0, r1);
-        count(grid, false, c0, c1);
+        count(grid, true, r);
+        count(grid, false, c);
 
         int m = grid.size();
         int n = grid[0].size();
@@ -35,7 +32,7 @@ public:
         for(int i = 0; i < m; i++) {
             vector<int> vec;
             for(int j = 0; j < n; j++) {
-                vec.push_back(r1[i] + c1[j] - r0[i] - c0[j]);
+                vec.push_back(2 * r[i] + 2 * c[j] - n - m);
             }
             diff.push_back(vec);
         }
