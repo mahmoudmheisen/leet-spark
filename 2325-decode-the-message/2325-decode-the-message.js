422601a41fb01a815edef95538e4df20957c1d69
mmh91 <mahmoudmheisen91@gmail.com>
2023-03-01 00:18:24 +0300

    Time: 60 ms (97.12%), Space: 44.8 MB (39.30%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.js
+++ 2325-decode-the-message/2325-decode-the-message.js
@@ -4,14 +4,15 @@
  * @return {string}
  */
 var decodeMessage = function(key, message) {
-    var keySet = new Set(key.split(" ").join(""));
-    var key2 = [...keySet].join("");
+    var keySet = new Set(key);
+    var newKey = [...keySet].filter(item => item !== " ");
     var letters = "abcdefghijklmnopqrstuvwxyz";
-    var ans = [];
-    for(var l of message) {
-        var a = key2.indexOf(l);
-        ans.push(a !== -1 ? letters[a] : " ")
+    var ans = "";
+    
+    for(var letter of message) {
+        var idx = newKey.indexOf(letter);
+        ans += (idx !== -1 ? letters[idx] : " ");
     }
     
-    return ans.join("");    
+    return ans;    
 };
