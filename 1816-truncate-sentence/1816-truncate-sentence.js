128f20cb78b6be68cd236e9aad4483554e8950e9
mmh91 <mahmoudmheisen91@gmail.com>
2023-03-01 17:32:38 +0300

    Time: 59 ms (87.79%), Space: 42 MB (61.60%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function(s, k) {
+    let arr = s.split(" ");
+    arr = arr.slice(0, k);
+    
+    return arr.join(" ");
+};
