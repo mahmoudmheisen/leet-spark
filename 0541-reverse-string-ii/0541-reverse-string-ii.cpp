da7a3bf6f33228c97e5a5015599a39da5e8477eb
mmh91 <mahmoudmheisen91@gmail.com>
2023-03-02 18:22:18 +0300

    Time: 8 ms (28.06%), Space: 7.6 MB (22.03%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.cpp
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
-    string sub(string s, int start, int end, bool flip = false) {
-        string res = "";
-
-        for(int j = start; j < end; j++) {
-            if(j < s.size()) res = flip ? s[j] + res : res + s[j];
-        }
-
-        return res;
-    }
-
     string reverseStr(string s, int k) {
-        string res = "";
-
-        for(int i = 0; i < s.size(); i+=2*k) { 
-            res += sub(s, i, i+k, true);
-            res += sub(s, i+k, i+2*k);
+        for( int i = 0; i < s.size(); i += 2*k ){
+            string str = s.substr(i, k);
+            reverse(str.begin(), str.end());
+            s.replace(i, k, str);
         }
-
-        return res; 
+        return s;
     }
 };
