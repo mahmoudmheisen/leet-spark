e4f11252b5df3ae28d0480d92bedd3174a4d78ba
mmh91 <mahmoudmheisen91@gmail.com>
2023-03-02 13:15:44 +0300

    Time: 16 ms (53.85%), Space: 14.4 MB (8.78%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -2,18 +2,31 @@ class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         int n = groupSizes.size();
-        vector<vector<int>> res, tempGroups(n + 1);
+        vector<vector<int>> res;
+        unordered_map<int, queue<int>> mapper;
         
-        for (auto i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int groupSize = groupSizes[i];
-            tempGroups[groupSize].push_back(i);
-            
-            if (tempGroups[groupSize].size() == groupSize) {
-                res.push_back(tempGroups[groupSize]);
-                tempGroups[groupSize] = {};
-            }
+            mapper[groupSize].push(i);
         }
-
+        
+        for (auto &it : mapper) {
+            int size = it.first;
+            queue<int> q = it.second;
+            
+            while(!q.empty()) {
+                vector<int> temp;
+                
+                for (int i = 0; i < size; i++) {
+                    temp.push_back(q.front());
+                    q.pop();
+                }
+                
+                res.push_back(temp);
+            }
+            
+        }
+        
         return res;
     }
 };
