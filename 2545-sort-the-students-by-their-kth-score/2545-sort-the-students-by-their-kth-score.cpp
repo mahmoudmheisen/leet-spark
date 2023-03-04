a3bd50ac0a5571decb9f2973b16c06239aef0ace
mmh91 <mahmoudmheisen91@gmail.com>
2023-03-04 23:14:53 +0300

    Time: 69 ms (87.96%), Space: 27.5 MB (82.48%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -1,21 +1,9 @@
 class Solution {
-private:
-    class MyComparator {
-        private:
-            int _k = 0;
-        
-        public:
-            MyComparator(int k) : _k(k) {};
-            bool operator() (vector<int> r1, vector<int> r2) {
-                return r1[_k] > r2[_k];
-            }
-    };
-    
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {   
-        MyComparator comp(k);
-        sort(score.begin(), score.end(), comp);
-        
+        for(auto &r : score) r.insert(r.begin(), r[k]);
+        sort(score.begin(), score.end(), greater<vector<int>>());
+        for(auto &r : score) r.erase(r.begin());
         return score;
     }
 };
