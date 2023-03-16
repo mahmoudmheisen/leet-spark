9a4d39bffee31a6984e626c9cbe1b94f69b329c1
mmh91 <mahmoudmheisen91@gmail.com>
2023-03-16 20:36:37 +0300

    Time: 103 ms (62.22%), Space: 8.9 MB (90.31%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int> res;
+        vector<int> ret(boxes.size(), 0);
         vector<int> idx;
+        int leftSum = 0, rightSum = 0;
+        int count = 0;
         
-        for(int i = 0; i < boxes.size(); i++) {
-            if(boxes[i] == '1') idx.push_back(i);
+        for (int i = 1; i < boxes.size(); i++) {
+            count += (boxes[i-1]-'0');
+            leftSum = leftSum + count;
+            cout << leftSum << " ";
+            ret[i] = leftSum;
+        }
+
+        count = 0;
+        for (int i = boxes.size()-2; i >= 0 ; i--) {
+            count += (boxes[i+1]-'0');
+            rightSum = rightSum + count;
+            cout << rightSum << " ";
+            ret[i] += rightSum;
         }
         
-        for(int i = 0; i < boxes.size(); i++) {
-            int b = boxes[i] - '0';
-            int sum = 0;
-            for(int s : idx) {
-                sum += abs(s - i);
-            }
-            
-            res.push_back(sum);
-        }
-        
-        return res;
+        return ret;
     }
 };
