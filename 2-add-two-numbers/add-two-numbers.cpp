ec577f2afe09a6094a74d236026e57a3d75dc1ed
mmh1 <mahmoudmheisen91@gmail.com>
2025-05-21 22:04:45 -0400

    Time: 3 ms (34.67%) | Memory: 77.2 MB (43.23%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -14,30 +14,24 @@ public:
         ListNode* current = new ListNode(0);
         ListNode* res = current;
 
-        int remainder = 0;
+        int sum = 0, curry = 0;
         while(l1 || l2) {
             int l1V = l1 ? l1->val : 0;
             int l2V = l2 ? l2->val : 0;
-            int sum = l1V + l2V + remainder;
+            int s = l1V + l2V + curry;
 
-            if(sum >= 10) {
-                int temp = sum % 10;
-                remainder = sum / 10;
-                sum = temp;
-            } else {
-                remainder = 0;
-            }
+            sum = s % 10;
+            curry = s / 10;
 
             current->next = new ListNode(sum);
 
             current = current->next;
-
             l1 = l1 ? l1->next: nullptr;
             l2 = l2 ? l2->next: nullptr;
         }
 
-        if(remainder) {
-            current->next = new ListNode(remainder);
+        if(curry) {
+            current->next = new ListNode(curry);
         }
 
         return res->next;
