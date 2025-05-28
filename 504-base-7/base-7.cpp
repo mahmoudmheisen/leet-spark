d6ebb11f3fb471f6ce4bc2538a98649c1c649afa
mmh1 <mahmoudmheisen91@gmail.com>
2025-05-28 04:36:08 -0400

    Time: 0 ms (100.00%) | Memory: 8.3 MB (31.27%) - LeetSync

--- 504-base-7/base-7.cpp
+++ 504-base-7/base-7.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     string convertToBase7(int num) {
         string base7Representation = "";
-        if (!num)
-            return "0";
+        
+        if (num == 0) return "0";
+        if (num < 0) return "-" + convertToBase7(-num);
 
-        bool isNegative = num < 0;
-        num = abs(num);
         while (num) {
             base7Representation = to_string(num % 7) + base7Representation;
-            num = num / 7;
+            num /= 7;
         }
 
-        return isNegative ? "-" + base7Representation : base7Representation;
+        return base7Representation;
     }
 };
