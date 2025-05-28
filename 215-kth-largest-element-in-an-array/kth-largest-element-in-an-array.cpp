74ff1bac8bc77a89824d818a68670934ccfec4f8
mmh1 <mahmoudmheisen91@gmail.com>
2025-05-28 15:55:12 -0400

    Time: 2168 ms (5.00%) | Memory: 59.3 MB (83.18%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,16 +1,37 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
+        return quickSelect(nums, 0, nums.size() - 1, k);
+    }
 
-        for (int i = 0; i < nums.size(); i++) {
-            pq.push(nums[i]);
+    int quickSelect(vector<int>& nums, int l, int r, int k) {
+        int n = nums.size();
+        if (l >= r)
+            return nums[l];
 
-            if (pq.size() > k) {
-                pq.pop();
+        int randIdx = rand() % (r - l) + l;
+        swap(nums[randIdx], nums[r]);
+        int p = partition(nums, l, r);
+
+        if (p > (n - k))
+            return quickSelect(nums, l, p - 1, k);
+        else if (p < n - k)
+            return quickSelect(nums, p + 1, r, k);
+        else
+            return nums[n - k];
+    }
+
+    int partition(vector<int>& nums, int l, int r) {
+        int p = nums[r];
+        int lo = l;
+
+        for (int i = l; i < r; i++) {
+            if (nums[i] < p) {
+                swap(nums[lo], nums[i]);
+                lo++;
             }
         }
-
-        return pq.top();
+        swap(nums[lo], nums[r]);
+        return lo;
     }
 };
