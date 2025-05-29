61a112802040a99af0b17e351a87b245ba589435
mmh1 <mahmoudmheisen91@gmail.com>
2025-05-28 21:59:35 -0400

    Time: 0 ms (100.00%) | Memory: 7.8 MB (74.86%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        unordered_set<int> ht;
+        int slow = n;
+        int fast = n;
 
+        while (1) {
+            slow = getNextNumber(slow);
+            fast = getNextNumber(getNextNumber(fast));
+
+            if (fast == 1)
+                return true;
+            if (fast == slow)
+                return false;
+        }
+
+        return false;
+    }
+
+    int getNextNumber(int n) {
         int res = 0;
         while (n) {
             int digit = n % 10;
             n /= 10;
             res += digit * digit;
-
-            if (n == 0 && res == 1)
-                return true;
-            else if (n == 0) {
-                n = res;
-                if (ht.count(res))
-                    return false;
-                ht.insert(res);
-                res = 0;
-            }
         }
 
-        return false;
+        return res;
     }
 };
