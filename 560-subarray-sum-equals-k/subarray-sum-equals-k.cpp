cb3be57355a1a3553ce5362a5ea535d3b308bda5
mmh1 <mahmoudmheisen91@gmail.com>
2025-06-02 04:46:00 -0400

    Time: 2206 ms (18.16%) | Memory: 37.3 MB (78.04%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -3,11 +3,11 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int count = 0;
         int n = nums.size();
-        vector<int> prefix(n+1, 0);
+        vector<int> prefix;
 
-        prefix[1] = nums[0];
-        for (int i = 2; i < n+1; i++) {
-            prefix[i] = prefix[i - 1] + nums[i-1];
+        prefix.push_back(0);
+        for (int i = 1; i < n+1; i++) {
+            prefix.push_back(prefix[i - 1] + nums[i-1]);
         }
 
         for (int i = 1; i < n + 1; i++) {
