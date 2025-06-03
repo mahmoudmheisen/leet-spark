15d55676aaadb2e582f04ea5e5da0842836294c9
mmh1 <mahmoudmheisen91@gmail.com>
2025-06-03 02:36:42 -0400

    Time: 2 ms (71.93%) | Memory: 18.2 MB (20.59%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,46 @@
+class Compare {
+public:
+    bool operator()(pair<string, int> a, pair<string, int> b)
+    {
+        if (a.second == b.second) {
+            return a.first < b.first;
+        }
+
+        return a.second > b.second;
+    }
+};
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<string> res;
+        unordered_map<string, int> ht;
+        priority_queue<pair<string, int>, vector<pair<string, int>>,
+                       Compare>
+            minHeap;
+
+        for (string word : words) {
+            if (ht.count(word))
+                ht[word]++;
+            else
+                ht[word] = 1;
+        }
+
+        for (auto pair: ht) {
+            minHeap.push(pair);
+
+            if(minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+
+        while(!minHeap.empty()) {
+            res.push_back(minHeap.top().first);
+            minHeap.pop();
+        }
+
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
+};
