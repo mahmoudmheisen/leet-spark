d8832e3ff82f58402ea0e313382e93d9a25bf01d
mmh1 <mahmoudmheisen91@gmail.com>
2025-06-03 14:51:43 -0400

    Time: 110 ms (5.36%) | Memory: 35.6 MB (5.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -4,45 +4,44 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        int count = 0;
+        int islandsCount = 0;
         for (int r = 0; r < m; r++) {
             for (int c = 0; c < n; c++) {
                 if (grid[r][c] - '0' == 1) {
                     dfs(grid, r, c);
-                    count++;
+                    islandsCount++;
                 }
             }
         }
 
-        return count;
-    }
+        return islandsCount;
+    };
 
     void dfs(vector<vector<char>>& grid, int r, int c) {
-        vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
+        // mark it as visited
         grid[r][c] = '2';
+        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        for (vector<int> dir : dirs) {
-            if (isValidLocation(grid, r, c, dir)) {
-                dfs(grid, r + dir[0], c + dir[1]);
+        for (auto [dx, dy] : dirs) {
+            vector<int> newCell = {dx + r, dy + c};
+            if (isValidCell(grid, newCell)) {
+                dfs(grid, newCell[0], newCell[1]);
             }
         }
     }
 
-    bool isValidLocation(vector<vector<char>>& grid, int r, int c,
-                         vector<int>& dir) {
-        vector<int> newLoc(2);
-        newLoc[0] = r + dir[0];
-        newLoc[1] = c + dir[1];
+    bool isValidCell(vector<vector<char>>& grid, vector<int>& newCell) {
         int m = grid.size();
         int n = grid[0].size();
 
-        if (newLoc[0] < 0 || newLoc[0] >= m)
+        // check the boundary
+        if (newCell[0] < 0 || newCell[0] >= m)
             return false;
-        if (newLoc[1] < 0 || newLoc[1] >= n)
+        if (newCell[1] < 0 || newCell[1] >= n)
             return false;
 
-        if (grid[newLoc[0]][newLoc[1]] - '0' != 1)
+        // check if valid
+        if (grid[newCell[0]][newCell[1]] - '0' != 1)
             return false;
 
         return true;
