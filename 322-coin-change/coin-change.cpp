8ff81f69c83e36947d4de1a8f6806bd6786baf1d
mmh1 <mahmoudmheisen91@gmail.com>
2025-06-03 22:56:40 -0400

    Time: 23 ms (69.60%) | Memory: 17.9 MB (81.85%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        unordered_map<int, int> memo;
-
-        int coinsCount = dp(coins, amount, memo);
-
-        return coinsCount == INT_MAX ? -1 : coinsCount;
-    }
-
-    int dp(vector<int>& coins, int amount, unordered_map<int, int>& memo) {
         if (amount == 0)
             return 0;
 
-        if (memo.count(amount))
-            return memo[amount];
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
 
-        int minCoin = INT_MAX;
-        for (int coin : coins) {
-            if (amount >= coin) {
-                int current = dp(coins, amount - coin, memo);
-                 minCoin = min(minCoin, current == INT_MAX ? INT_MAX : current + 1);
+        for (int t = 1; t < amount + 1; t++) {
+            for (int coin : coins) {
+                if (t >= coin) {
+                    dp[t] =
+                        min(dp[t], dp[t - coin] == INT_MAX ? INT_MAX
+                                                           : dp[t - coin] + 1);
+                }
             }
         }
 
-        memo[amount] = minCoin;
-        return minCoin;
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
