7702a26f6038c02ec201876940b25ba49e413ec1
mmh1 <mahmoudmheisen91@gmail.com>
2025-06-08 03:36:10 -0400

    Time: 4 ms (31.81%) | Memory: 8.6 MB (30.96%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -5,27 +5,15 @@ public:
         int word1Pointer = 0;
         int word2Pointer = 0;
 
-        for (int i = 0; i < word1.size() + word2.size(); i++) {
-            string currentWord = word1;
-            int pointer = 0;
-            if (i & 1) {
-                if (word2Pointer < word2.size()) {
-                    currentWord = word2;
-                    pointer = word2Pointer++;
-                } else {
-                    currentWord = word1;
-                    pointer = word1Pointer++;
-                }
-            } else {
-                if (word1Pointer < word1.size()) {
-                    currentWord = word1;
-                    pointer = word1Pointer++;
-                } else {
-                    currentWord = word2;
-                    pointer = word2Pointer++;
-                }
-            }
-            mergedString += currentWord[pointer];
+        for (int i = 0; i < min(word1.size(), word2.size()); i++) {
+            mergedString += word1[i];
+            mergedString += word2[i];
+        }
+
+        if (word1.size() < word2.size()){
+            mergedString += word2.substr(word1.size());
+        } else {
+            mergedString += word1.substr(word2.size());
         }
 
         return mergedString;
